52dd11c0514b6f333af55bfbbc0b5d9711b3e390
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-08-19 16:38:03 -0300

    first commit: Fast React Pizza

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import AppLayout from "./ui/AppLayout";
+import Home from "./ui/Home";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+    {
+        element: <AppLayout />,
+        errorElement: <Error />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+
+            },
+            {
+                path: "/menu",
+                element: <Menu />,
+                errorElement: <Error />,
+                loader: menuLoader
+            },
+            {
+                path: "/cart",
+                element: <Cart />
+            },
+            {
+                path: "/order/new",
+                element: <CreateOrder />
+            },
+            {
+                path: "/order/:orderId",
+                element: <Order />
+            }
+        ]
+    },
+
+]);
+
+function App() {
+    return (
+        <RouterProvider router={router} />
+    )
+}
+
+export default App

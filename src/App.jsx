9420d0b4362322ef9b3f9a67afa17aac141f9d49
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-08-21 11:53:53 -0300

    feat: Creating new Order

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, { action as createOrderAction } from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
 
 const router = createBrowserRouter([
@@ -30,6 +30,7 @@ const router = createBrowserRouter([
             },
             {
                 path: "/order/new",
+                action: createOrderAction,
                 element: <CreateOrder />
             },
             {

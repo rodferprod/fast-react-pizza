b3c6b1cc28fe2de84c6970f18ee63f83ff481210
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-08-21 16:22:08 -0300

    feat: Starting to styling components with TailwindCSS

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,13 +1,13 @@
 function Home() {
-  return (
-    <div>
-      <h1>
-        The best pizza.
-        <br />
-        Straight out of the oven, straight to you.
-      </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className="text-xl font-semibold text-center text-stone-700">
+                The best pizza.
+                <br />
+                <span className="text-yellow-500">Straight out of the oven, straight to you.</span>
+            </h1>
+        </div>
+    );
 }
 
 export default Home;

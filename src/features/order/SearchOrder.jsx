fde1fe5661b76c35e5aa091d70ecb4ea662ec729
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-08-22 01:05:25 -0300

    feat: Styling Form Elements

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -16,6 +16,7 @@ function SearchOrder() {
     return (
         <form onSubmit={handleSubmit}>
             <input
+                className="px-4 py-2 text-sm duration-300 bg-yellow-100 rounded-full w-28 sm:w-64 sm:focus:w-72 placeholder:text-stone-400 transition:all focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50"
                 type="text"
                 placeholder="Filter orders..."
                 value={query}

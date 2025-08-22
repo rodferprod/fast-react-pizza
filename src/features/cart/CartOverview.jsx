9a799e6c772a91e3ec06220d9c585003e4dbc10f
RodrigoJGF <rodrigoferreira.tico@gmail.com>
2025-08-22 00:14:39 -0300

    feat: Flex Box Layout with TailwindCSS

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
     return (
-        <div className="px-4 py-4 text-sm uppercase md:text-base sm:px-6 bg-stone-800 text-stone-200">
+        <div className="flex items-center justify-between px-4 py-4 text-sm uppercase md:text-base sm:px-6 bg-stone-800 text-stone-200">
             <p className="space-x-4 font-semibold sm:space-x-6 text-stone-300">
                 <span>23 pizzas</span>
                 <span>$23.45</span>
